ee5caa7d422e549b0b2f08bbaa7f3eeec3a2236a
jaroslaw-filipiak <info@j-filipiak.pl>
2021-08-18 12:24:27 +0200

    reveal js library

--- main.js
+++ main.js
@@ -1,3 +1,51 @@
 import './src/index.scss'
 
 
+import Reveal from 'reveal.js';
+import Markdown from 'reveal.js/plugin/markdown/markdown.esm.js';
+
+
+
+
+window.addEventListener('resize', () => {
+    let intViewportWidth =  document.documentElement.clientWidth;
+
+    if(intViewportWidth > 1280) {
+        let deck = new Reveal({
+            plugins: [ Markdown ]
+         })
+         deck.initialize({
+             width: '100%',
+             height: '100%',
+             minScale: 0.2,
+             maxScale: 1.0,
+             margin: 0.01,
+             disableLayout: true,
+             touch: false
+         });
+    }
+
+  
+
+})
+
+window.addEventListener('DOMContentLoaded', () => {
+   
+    if(intViewportWidth > 1280) {
+        let deck = new Reveal({
+            plugins: [ Markdown ]
+         })
+         deck.initialize({
+             width: '100%',
+             height: '100%',
+             minScale: 0.2,
+             maxScale: 1.0,
+             margin: 0.01,
+             disableLayout: true,
+             touch: false
+         });
+    }
+
+    
+})
+
